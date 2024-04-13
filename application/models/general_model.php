b480df0f1071bfb9e6623e18aaaedb69543ea5a0
Nuski Mohamed <nuskirauf@gmail.com>
2024-04-14 00:29:36 +0530

    ch

--- application/models/general_model.php
+++ application/models/general_model.php
@@ -3,15 +3,15 @@ class General_model extends CI_Model{
 	function fetch_config($branch_auto_id,$company_auto_id){
     	$data_arr = array();
         $this->load->driver('cache',array('adapter' => 'apc', 'backup' => 'file'));
-        if (!$data_arr = $this->cache->get("00000{$branch_auto_id}_{$company_auto_id}")){
-            $this->db->select('*');
-        	$this->db->from('config'); 
-        	$this->db->where('company_auto_id', $company_auto_id);  
-        	$data_arr = $this->db->get()->row_array();
-			$this->cache->save("00000{$branch_auto_id}_{$company_auto_id}", $data_arr, 30000);
-            echo "please discuss with nuski";
-            exit();//please discuss with nuski
-        }
+        // if (!$data_arr = $this->cache->get("00000{$branch_auto_id}_{$company_auto_id}")){
+        //     $this->db->select('*');
+        // 	$this->db->from('config'); 
+        // 	$this->db->where('company_auto_id', $company_auto_id);  
+        // 	$data_arr = $this->db->get()->row_array();
+		// 	$this->cache->save("00000{$branch_auto_id}_{$company_auto_id}", $data_arr, 30000);
+        //     echo "please discuss with nuski";
+        //     exit();//please discuss with nuski
+        //}
         return $data_arr;
     }
 }
